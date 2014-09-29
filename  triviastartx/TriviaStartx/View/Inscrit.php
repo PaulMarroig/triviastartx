83f78b8aef0d48c0f77234cf74aeb853b9f40a21
pmarroig@sts-sio-caen.info <pmarroig@sts-sio-caen.info@ce357274-bddd-80b8-b8c8-d47f0e5f0021>
2014-09-29 07:15:27 +0000

    remodif

---
+++  triviastartx/TriviaStartx/View/Inscrit.php
@@ -0,0 +1,13 @@
+<center>
+		<label>login</label>
+			<input type="text" name="pseudo" value="<?php if (isset($comment)) echo htmlspecialchars($comment['auteur']); ?>" /><br />
+		<label>nom</label>
+			<input type="text" name="pseudo" value="<?php if (isset($comment)) echo htmlspecialchars($comment['auteur']); ?>" /><br />
+		<label>prenom</label>
+			<input type="text" name="pseudo" value="<?php if (isset($comment)) echo htmlspecialchars($comment['auteur']); ?>" /><br />
+		<label>mail</label>
+			<input type="text" name="pseudo" value="<?php if (isset($comment)) echo htmlspecialchars($comment['auteur']); ?>" /><br />
+		<label>password</label>
+			<input type="text" name="pseudo" value="<?php if (isset($comment)) echo htmlspecialchars($comment['auteur']); ?>" /><br />
+		 <input type="submit" value="S'inscrire" />
+	</center>
